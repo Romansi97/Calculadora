27832e198346cfa9c95bcb3fc0275d8019cd6735
Roman Ricciardi <romanezequielr97@gmail.com>
2023-09-23 11:08:30 -0300

    contenedor-calculadora

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           className="freecodecamp-logo"
           alt="Logo de freeCodeCamp"
         />
+        <div className="contenedor-calculadora"></div>
       </div>
     </div>
   );
